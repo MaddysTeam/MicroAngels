5dd2b014fb22b49a44823b583c4b05b7ffaa4854
JimmyPoor <kissnana3@163.com>
2019-07-09 23:20:08 +0800

    add new site

---
+++ site/MicroAngels.Site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MicroAngels.Site.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
