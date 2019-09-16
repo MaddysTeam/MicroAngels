7352980f990dd741818d35bc6441a2b4c475bcd3
JimmyPoor <kissnana3@163.com>
2019-09-17 00:05:43 +0800

    add apollo configuration

---
+++ src/MicroAngels.Configuration/Apollo/ApolloExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Com.Ctrip.Framework.Apollo;
+using Microsoft.Extensions.Configuration;
+
+namespace MicroAngels.Configuration.Apollo
+{
+
+	public static class ApolloExtensions
+	{
+
+		public static IConfigurationBuilder AddApolloConfiguration(this IConfigurationBuilder builder, ApolloOptions options)
+		{
+			var apolloBuilder=builder.AddApollo(options)
+				.AddDefault();
+
+			foreach (var @namespace in options.Namespace)
+			{
+				apolloBuilder.AddNamespace(@namespace);
+			}
+
+			return builder;
+		}
+
+	}
+
+}
---
+++ src/MicroAngels.Configuration/Apollo/ApolloOptions.cs
@@ -0,0 +1,30 @@
+﻿using Com.Ctrip.Framework.Apollo;
+using Com.Ctrip.Framework.Apollo.Enums;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace MicroAngels.Configuration.Apollo
+{
+
+	public class ApolloOptions:IApolloOptions
+	{
+		public string AppId { get; set; }
+		public string MetaServer { get; set; }
+		public string[] Namespace { get; set; }
+
+		public string DataCenter { get; set; }
+		public string Cluster { get; set; }
+		public Env Env { get; set; }
+		public string SubEnv { get; set; }
+		public string LocalIp { get; set; }
+		public IReadOnlyCollection<string> ConfigServer { get; set; }
+		public int Timeout { get; set; }
+		public string Authorization { get; set; }
+		public int RefreshInterval { get; set; }
+		public string LocalCacheDir { get; set; }
+		public Func<HttpMessageHandler> HttpMessageHandlerFactory => throw new NotImplementedException();
+	}
+
+}
